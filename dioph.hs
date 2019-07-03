3717be93345cf995e27387ed1e9af998630c916f
AndrewClimber <seoclimber@gmail.com>
2019-07-03 18:04:55 +0700

    03.07.2019

--- dioph.hs
+++ dioph.hs
@@ -1,4 +1,5 @@
 {-
+5 kuy. Diophantine Equation
 In mathematics, a Diophantine equation is a polynomial equation, usually with two or more unknowns, 
 such that only the integer solutions are sought or studied.
 In this kata we want to find all integers x, y (x >= 0, y >= 0) solutions of a diophantine equation of the form:
