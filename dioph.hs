9230245a381b3b314f44d25cd498c1f05b30230c
AndrewClimber <seoclimber@gmail.com>
2019-07-03 09:37:40 +0700

    #

---
+++ dioph.hs
@@ -0,0 +1,35 @@
+{-
+In mathematics, a Diophantine equation is a polynomial equation, usually with two or more unknowns, 
+such that only the integer solutions are sought or studied.
+In this kata we want to find all integers x, y (x >= 0, y >= 0) solutions of a diophantine equation of the form:
+x2 - 4 * y2 = n
+(where the unknowns are x and y, and n is a given positive number) in decreasing order of the positive xi.
+If there is no solution return [] or "[]" or "". (See "RUN SAMPLE TESTS" for examples of returns).
+Examples:
+solEquaStr(90005) --> "[[45003, 22501], [9003, 4499], [981, 467], [309, 37]]"
+solEquaStr(90002) --> "[]"
+Hint:
+x2 - 4 * y2 = (x - 2*y) * (x + 2*y)
+-}
+module Codewars.Kata.Dioph where
+    import Data.List
+    
+    
+    solequa :: Integer -> [(Integer, Integer)]
+    solequa n = tail $ nub  [if (x - 2*y) * (x + 2*y) == n then (x,y) else (0,0)  | x <- [0..((n `div` 2)+10)], y <- [0..((n `div` 2)+10)]]
+    
+    sol n  = tail $ nub  [if (x - 2*y) * (x + 2*y) == n then (x,y) else (0,0)  | x <- [0..100], y <- [0..100] ]
+    
+    -- sol1 n = scanr (\x y ->if (x - 2*y) * (x + 2*y) == n then (x,y) else (0,0))  0
+    sol1 n  = tail $ nub  [if (x - 2*y) * (x + 2*y) == n then (x,y) else (0,0)  | x <- (deli n), y <- (deli n) ]
+    
+    sol2 n  = tail $ nub  [if (x - 2*y) * (x + 2*y) == n then (x,y) else (0,0)  | x <- [0,1,5], y <- [0,1,5] ]
+    
+    sol3 n  =  [if ((x k1 k2) - 2*(y k1 k2)) * ((x k1 k2) + 2*(y k1 k2)) == n then ((x k1 k2),(y k1 k2)) else (0,0) | k1 <- (deli n), k2 <- (deli n)] 
+                          where
+                               x k1 k2 = (k2+k1)/2
+                               y k1 k2 = (k2-k1)/4
+    
+    sol4 n  =  [if (((k2+k1)/2) - 2*((k2-k1)/4)) * (((k2+k1)/2) + 2*((k2-k1)/4)) == n then (((k2+k1)/2),((k2-k1)/4)) else (0,0) | k1 <- (deli n), k2 <- (deli n)] 
+    
+    deli n = nub [if snd (n `divMod` x) == 0 then fst (n `divMod` x) else 0 | x <- [1..n]]
